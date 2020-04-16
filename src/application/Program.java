c3ab79a819d52a5d3a4b51a57e6db450029c766e
otenfa <afneto1960@gmail.com>
2020-04-16 15:37:45 -0300

    Inclusao exemplos de Dowcasting e Upcasting

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,42 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
+		//UPCASTING
 		
-
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+		
+		// DOWCASTING
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
+		
+		//BusinessAccount acc5 = (BusinessAccount)acc3;
+		// atenção na hora de dowcasting, nem sempre é possível e o compilador só recalma na execução!!! 
+		// o programador tem que estar atento.
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("loan!" );
+		}
+		
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+		
 	}
 
 }
